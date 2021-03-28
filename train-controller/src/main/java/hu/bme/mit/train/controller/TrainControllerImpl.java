16520ac6cda4bd22ef7d2aaf517697c927d26f83
Hedo09 <hedrichadam09@gmail.com>
2021-03-28 23:00:23 +0200

    Thread kiegészítés

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,6 +7,11 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	Thread thread = new Thread(new ThreadRunnable());
+
+	public TrainControllerImpl() {
+	    thread.start();
+    }
 
 	@Override
 	public void followSpeed() {
@@ -46,4 +51,18 @@ public class TrainControllerImpl implements TrainController {
 		this.step = joystickPosition;		
 	}
 
+    private class ThreadRunnable implements Runnable {
+        @Override
+        public void run() {
+            while(!Thread.currentThread().isInterrupted()){
+                followSpeed();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
